b99edba89aaba09e08fd0142c9695a8fddd72cb0
James Haggerty <jpm.haggerty@gmail.com>
2021-11-18 16:44:48 -0500

    Co-authored-by: LindsayFarias <LindsayFarias@users.noreply.github.com>

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { Grid } from '@mui/material';
 
-const apiCall = async (url) => {
+const baseURL = 'https://localhost:3000';
+
+const Homepage = () => {
+  const [LxCriteria, setLxCriteria] = useState(null);
+
+  const apiCall = async (url) => {
     const result = await fetch(baseURL + url);
     const data = await result.json();
     return data;
 };
 
-const Homepage = () => {
-  const [LxCriteria, setLxCriteria] = useState(null);
+  const getCriteria = async () => {
+    const lightningData = await apiCall('/lightning');
+    setLxCriteria(lightningData);
 
-  console.log(apiCall);
-  const getLxCritera = async () => {
-    let data = await apiCall('/lightning');
-    setLxCriteria(data);
+    let cumulusData = await apiCall('/cumulusCloud');
+    setLxCriteria(lightningData);
   };
 
   useEffect(() => {
-    getLxCriteria();
+    getCriteria();
   }, []);
 
   return (
